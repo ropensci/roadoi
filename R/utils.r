8b80e6cbd36c851ea5349b41aebfbbf742ae374b
Najko Jahn <najko.jahn@gmail.com>
2017-02-22 11:29:54 +0100

    bumpt to new API version

--- R/utils.r
+++ R/utils.r
@@ -1,6 +1,6 @@
 # meta
 oadoi_baseurl <- function() "https://api.oadoi.org/"
-oadoi_api_version <- function() "1.1.0"
+oadoi_api_version <- function() "1.2.0"
 api_limit <- 10000
 
 # user agent, so oaDOI can track the usage of this client
